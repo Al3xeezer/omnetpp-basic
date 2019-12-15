14f0d8456a6b0485458c6617af133fbee538bb03
Al3xeezer <alesnuneztarilonte@gmail.com>
2019-12-15 14:36:14 +0100

    Updated source.cc to use pck instead of msg

--- source.cc
+++ source.cc
@@ -1,9 +1,12 @@
-// This file is distributed WITHOUT ANY WARRANTY. See the file
-// `license' for details on this and other legal matters.
-
 
 #include <string.h>
 #include <omnetpp.h>
+#include "myPacket_m.h"  // Use "myPacket" packet structure
+
+/*Type definitions for myPacket*/
+#define TYPE_PCK 0
+#define TYPE_ACK 1
+#define TYPE_NACK 2
 
 using namespace omnetpp;
 
@@ -11,12 +14,14 @@ using namespace omnetpp;
 class source : public cSimpleModule
 {
   protected:
-    // The following redefined virtual function holds the algorithm.
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
+    virtual myPacket *createPck();
 
   private:
     simtime_t meanTime;
+    myPacket *firstPck;
+    unsigned int seq;
 };
 
 // The module class needs to be registered with OMNeT++
@@ -24,26 +29,47 @@ Define_Module(source);
 
 void source::initialize()
 {
-    // Send packet to itself after meanTime (lambda=1/meanTime)
+    /*Send packet to itself after meanTime (lambda=1/meanTime)*/
     meanTime=1/2;
-    cMessage *msg = new cMessage("tictocMsg");
 
-    scheduleAt(meanTime,msg);
+     /*Initialize sequence number*/
+    seq=1;
+
+//    cMessage *msg = new cMessage("tictocMsg");
+//    scheduleAt(meanTime,msg);
+
+    firstPck = new myPacket();
+    scheduleAt(meanTime, firstPck);
 
 }
 
 void source::handleMessage(cMessage *msg)
 {
-    // The handleMessage() method is called whenever a message arrives
-    // at the module. Here, when an event is receiver, it sends the packet
-    // and creates another event in X seconds. Like a loop.
+/*     The handleMessage() method is called whenever a message arrives
+     at the module. Here, when an event is received, it sends the packet
+     and creates another event in X seconds. Like a loop.
 
-    // exponential(): Generates random numbers from the exponential distribution.
+     exponential(): Generates random numbers from the exponential distribution.*/
 
-    send(msg, "out");
+//    send(msg, "out");
+//    cMessage *msg2 = new cMessage("basicMsg");
+//    scheduleAt(simTime()+exponential(meanTime),msg2);
 
-    cMessage *msg2 = new cMessage("basicMsg");
-    scheduleAt(simTime()+exponential(meanTime),msg2);
+    myPacket *pck = createPck();
+    send(pck, "out");
 
+    /*I use firstPck as the handleMessage trigger to make periodic events*/
+    scheduleAt(simTime()+exponential(meanTime),firstPck);
+
+}
+
+myPacket *source::createPck() /*Returns the pointer of the created pck*/
+{
+    myPacket *pck = new myPacket();
+    pck->setSeq(seq++);
+    pck->setType(TYPE_PCK);
+    pck->setBitLength(1024);
+
+    return pck;
 }
 
